1a424f7ada6535049586c61569fb5fb6252f36b3
Dikki Setiawan <initialsds@gmail.com>
2023-09-24 18:45:10 +0700

    updated webhook

--- src/services/webhook/webhook.controller.ts
+++ src/services/webhook/webhook.controller.ts
@@ -22,4 +22,44 @@ export class WebhookController {
 
     return;
   }
+
+  @Post('incoming') // route for incoming WhatsApp messages
+  async receiveIncomingWhatsAppMessage(@Req() request): Promise<void> {
+    const requestBody = request.body;
+
+    // Extract data from the incoming WhatsApp message payload
+    const {
+      id,
+      pushName,
+      isGroup,
+      group,
+      message,
+      phone,
+      messageType,
+      file,
+      mimeType,
+      deviceId,
+      sender,
+      timestamp,
+    } = requestBody;
+
+    // Handle the incoming WhatsApp message data here
+    console.log('Received WhatsApp Incoming Message:');
+    console.log(`ID: ${id}`);
+    console.log(`Push Name: ${pushName}`);
+    console.log(`Is Group: ${isGroup}`);
+    console.log(`Message: ${message}`);
+    console.log(`Phone: ${phone}`);
+    console.log(`Message Type: ${messageType}`);
+    console.log(`File: ${file}`);
+    console.log(`MIME Type: ${mimeType}`);
+    console.log(`Device ID: ${deviceId}`);
+    console.log(`Sender: ${sender}`);
+    console.log(`Timestamp: ${timestamp}`);
+
+    // You can add your logic here to process the incoming message as needed.
+    // For example, you can store it in a database or trigger further actions.
+
+    return;
+  }
 }
