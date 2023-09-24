312088e1227a19d353c13d2a3c9472982e8636c9
Dikki Setiawan <initialsds@gmail.com>
2023-09-24 18:23:40 +0700

    updated webhook

--- src/services/webhook/webhook.controller.ts
+++ src/services/webhook/webhook.controller.ts
@@ -6,34 +6,20 @@ export class WebhookController {
   async trackWhatsAppMessage(@Req() request): Promise<void> {
     const requestBody = request.body;
 
-    const {
-      id,
-      pushName,
-      isGroup,
-      group,
-      message,
-      phone,
-      messageType,
-      file,
-      mimeType,
-      deviceId,
-      sender,
-      timestamp,
-    } = requestBody;
+    // Extract data from the WhatsApp webhook payload
+    const { id, status, phone, note, deviceId } = requestBody;
 
-    // Handle the WhatsApp message data here
-    console.log('Received WhatsApp Message:');
+    // Handle the WhatsApp message status data here
+    console.log('Received WhatsApp Message Status Update:');
     console.log(`ID: ${id}`);
-    console.log(`Push Name: ${pushName}`);
-    console.log(`Is Group: ${isGroup}`);
-    console.log(`Message: ${message}`);
+    console.log(`Status: ${status}`);
     console.log(`Phone: ${phone}`);
-    console.log(`Message Type: ${messageType}`);
-    console.log(`File: ${file}`);
-    console.log(`MIME Type: ${mimeType}`);
+    console.log(`Note: ${note}`);
     console.log(`Device ID: ${deviceId}`);
-    console.log(`Sender: ${sender}`);
-    console.log(`Timestamp: ${timestamp}`);
+
+    // You can add your logic here to process the status update as needed.
+    // For example, you can store it in a database or trigger further actions.
+
     return;
   }
 }
