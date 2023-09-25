2f23d747d6aeee7a35d955acce61cbf950cab685
Dikki Setiawan <initialsds@gmail.com>
2023-09-25 14:07:45 +0700

    updated webhook module

--- src/services/webhook/webhook.module.ts
+++ src/services/webhook/webhook.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { WebhookController } from './webhook.controller';
 import { WhatsappStatusModule } from '../whatsapp-status/whatsapp-status.module';
 import { GuestModule } from '../guest/guest.module';
+import { WhatsappGatewayModule } from '../whatsapp-gateway/whatsapp-gateway.module';
 
 @Module({
   controllers: [WebhookController],
-  imports: [WhatsappStatusModule, GuestModule, WhatsappStatusModule],
+  imports: [
+    WhatsappStatusModule,
+    GuestModule,
+    WhatsappStatusModule,
+    WhatsappGatewayModule,
+  ],
 })
 export class WebhookModule {}
