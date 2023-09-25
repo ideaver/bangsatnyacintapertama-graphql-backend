5390357a094d614e26c82330522bbad524829076
Dikki Setiawan <initialsds@gmail.com>
2023-09-25 17:11:13 +0700

    event listener problem

--- src/app.module.ts
+++ src/app.module.ts
@@ -49,7 +49,22 @@ import { WebhookModule } from './services/webhook/webhook.module';
     }),
     //for shceduler
     SchedulerModule,
-    EventEmitterModule.forRoot(),
+    EventEmitterModule.forRoot({
+      // set this to `true` to use wildcards
+      // wildcard: false,
+      // // the delimiter used to segment namespaces
+      // delimiter: '.',
+      // // set this to `true` if you want to emit the newListener event
+      // newListener: false,
+      // // set this to `true` if you want to emit the removeListener event
+      // removeListener: false,
+      // the maximum amount of listeners that can be assigned to an event
+      maxListeners: 1,
+      // show event name in memory leak message when more than maximum amount of listeners is assigned
+      verboseMemoryLeak: true,
+      // disable throwing uncaughtException if an error event is emitted and it has no listeners
+      ignoreErrors: false,
+    }),
     ConfigsModule,
     // EmailModule,
     UploaderModule,
